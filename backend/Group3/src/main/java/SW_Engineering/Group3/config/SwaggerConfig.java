d941ecd8f66c8c93f0243c26375fcaa222fc95a7
WooJin Kim <dnwls813@ajou.ac.kr>
2022-11-02 00:28:24 +0900

    [Add]: Swagger-UI 추가
    - API 문서를 자동으로 만들어주는 툴입니다.
    - 입력 파라미터나 응답 JSON 데이터 형식을 한눈에 볼 수 있습니다.
    - localhost:8080/swagger로 접속할 수 있습니다.

---
+++ backend/Group3/src/main/java/SW_Engineering/Group3/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package SW_Engineering.Group3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false) // Swagger에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 노출 x
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("SW_Engineering.Group3.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo()); // swagger UI로 노출할 정보
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger Test")
+                .description("SwaggerConfig")
+                .version("3.0")
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+}
