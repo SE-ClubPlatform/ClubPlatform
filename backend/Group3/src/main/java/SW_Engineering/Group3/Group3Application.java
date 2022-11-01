e4dbf0fdeb4f17f0ba2867ccb1e393b9d16c4497
WooJin Kim <dnwls813@ajou.ac.kr>
2022-11-02 00:16:49 +0900

    [Add] : 프로젝트 기본 설정

---
+++ backend/Group3/src/main/java/SW_Engineering/Group3/Group3Application.java
@@ -0,0 +1,13 @@
+package SW_Engineering.Group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3Application.class, args);
+	}
+
+}
---
+++ backend/Group3/src/test/java/SW_Engineering/Group3/Group3ApplicationTests.java
@@ -0,0 +1,21 @@
+package SW_Engineering.Group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3ApplicationTests {
+
+	@Nested
+	class AuthenticationTest {
+		@Test
+		void signUpTest(){
+
+		}
+
+		@Test
+		void loginTest(){
+
+		}
+	}
+}
